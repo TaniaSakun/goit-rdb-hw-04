69c3a4988042cd52ed12c146b25f8b5a6a8dbf11
Tetiana Sakun <>
2024-12-03 23:14:22 +0100

    Add changes after review

--- dz_4.sql
+++ dz_4.sql
@@ -1,3 +1,4 @@
+-- Task 1 Create a database for the book library
 CREATE DATABASE LibraryManagement;
 USE LibraryManagement;
 
@@ -37,6 +38,7 @@ CREATE TABLE borrowed_books (
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
+-- Task 2 Fill the tables with simple, fictional test data.
 INSERT INTO authors (author_name) VALUES 
 ('J.K. Rowling'),
 ('George R.R. Martin');
@@ -61,6 +63,7 @@ INSERT INTO borrowed_books (book_id, user_id, borrow_date, return_date) VALUES
 (1, 1, '2024-11-01', '2024-11-10'), (2, 2, '2024-11-15', NULL);
 SELECT * FROM borrowed_books;
 
+-- Task 3: Combine All Tables Using FROM and INNER JOIN
 USE dz_3_Sakun;
 
 SELECT *
@@ -73,6 +76,7 @@ INNER JOIN employees e ON o.employee_id = e.employee_id
 INNER JOIN shippers s ON o.shipper_id = s.id
 INNER JOIN suppliers sup ON p.supplier_id = sup.id;
 
+-- Task 4 Run the queries
 SELECT COUNT(*) AS row_count
 FROM order_details od
 INNER JOIN orders o ON od.order_id = o.id
@@ -83,11 +87,21 @@ INNER JOIN employees e ON o.employee_id = e.employee_id
 INNER JOIN shippers s ON o.shipper_id = s.id
 INNER JOIN suppliers sup ON p.supplier_id = sup.id;
 
-SELECT COUNT(*) AS row_count
-FROM order_details od
-LEFT JOIN orders o ON od.order_id = o.id
-LEFT JOIN customers c ON o.customer_id = c.id
-LEFT JOIN products p ON od.product_id = p.id;
+SELECT COUNT(*) 
+FROM orders
+INNER JOIN order_details on orders.id = order_details.order_id
+LEFT JOIN products on order_details.product_id = products.id
+RIGHT JOIN customers on orders.customer_id = customers.id
+LEFT JOIN categories on products.category_id = categories.id
+LEFT JOIN suppliers on products.supplier_id = suppliers.id
+LEFT JOIN employees on orders.employee_id = employees.employee_id
+LEFT JOIN shippers on orders.shipper_id = shippers.id;
+
+-- When performing various LEFT/RIGHT JOIN combinations, the total number of rows returned was 535, 
+-- which is more than the number of products in all orders (records from the order_details table). 
+-- This is because the customers table contains customers who did not place orders. Using a RIGHT JOIN 
+-- on the customers table resulted in all records from this table being included in the result set, even 
+-- those that did not have a match in other tables.
 
 SELECT *
 FROM employees
@@ -101,7 +115,6 @@ FROM order_details od
 INNER JOIN products p ON od.product_id = p.id
 INNER JOIN categories cat ON p.category_id = cat.id
 GROUP BY cat.name;
-
 
 SELECT 
     cat.name AS category_name,
@@ -135,4 +148,3 @@ GROUP BY cat.name
 HAVING AVG(od.quantity) > 21
 ORDER BY row_count DESC
 LIMIT 4 OFFSET 1;
-
